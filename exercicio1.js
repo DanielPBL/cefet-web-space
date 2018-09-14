230e2fe88175c9833f37e4df1709a4044f41444e
Daniel <desenv00@cosmos>
2018-09-14 16:33:56 -0300

    Prática JS

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,11 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+const buttonsEl = document.querySelectorAll('.botao-expandir-retrair');
+for (const buttonEl of buttonsEl) {
+  buttonEl.addEventListener('click', event => {
+    const pEl = event.target.parentNode;
+
+    pEl.classList.toggle('expandido');
+    event.target.innerHTML = event.target.innerHTML === '+' ? '-' : '+';
+  });
+}
--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,17 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+const imgEl = document.querySelector('#slide');
+let indexAtual = 0;
+
+function rolarImagens(inc) {
+  if (indexAtual === 0 && inc < 0) {
+    indexAtual = todasAsImagens.length;
+  }
+  indexAtual += inc;
+  imgEl.src = servidorDasImagens + todasAsImagens[indexAtual % todasAsImagens.length];
+}
+
+document.querySelector('#anterior').addEventListener('click', () => {rolarImagens(-1);});
+
+document.querySelector('#proximo').addEventListener('click', () => {rolarImagens(1);});
